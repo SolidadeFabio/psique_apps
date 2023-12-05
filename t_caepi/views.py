5d1ff39699a0a2266f3ae7ace905aae536d038f0
Fabio Solidade <solidadefab@gmail.com>
2023-12-05 17:23:04 -0300

    alteracoes view

--- t_caepi/views.py
+++ t_caepi/views.py
@@ -7,18 +7,35 @@ from django.core.files.base import ContentFile
 
 
 def caepi(request, ca):
+    
     try:
-        caepi = CaEPI.objects.get(ca_number=ca)
-        print('achou')
+        ca_busca = float(ca)
         
-        if caepi.validade < date.today():
+        caepi = CaEPI.objects.filter(ca_number=ca_busca).first()
+        if not caepi:
             raise CaEPI.DoesNotExist
         
+        if caepi.validade < date.today():
+            file_content, data = download_certificado(ca=ca)
+            c_a = data['CA']
+            validade = datetime.strptime(data['Validade'], '%d/%m/%Y').strftime('%Y-%m-%d')
+            
+            if file_content:
+                file_content_file = ContentFile(file_content)
+
+                caepi.ca_number = c_a
+                caepi.validade = validade
+                caepi.pdf.save(f'CA{ca}.pdf', file_content_file)
+                
+                response = HttpResponse(caepi.pdf, content_type='application/pdf')
+                response['Content-Disposition'] = f'attachment; filename="{caepi.pdf.name}"'
+                return response
         response = HttpResponse(caepi.pdf, content_type='application/pdf')
         response['Content-Disposition'] = f'attachment; filename="{caepi.pdf.name}"'
         return response
-    
     except CaEPI.DoesNotExist:
+        
+        
         file_content, data = download_certificado(ca=ca)
         
         c_a = data['CA']
@@ -27,6 +44,7 @@ def caepi(request, ca):
         
         if file_content:
             file_content_file = ContentFile(file_content)
+            
             caepi = CaEPI(
                 ca_number=c_a,
                 validade=validade,
