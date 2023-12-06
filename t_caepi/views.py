1527d4ba0b53dbe79e24d36ef8675f67881c7f5b
Fabio Solidade <solidadefab@gmail.com>
2023-12-06 15:08:59 -0300

    aleracoes tecnicos

--- t_caepi/views.py
+++ t_caepi/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import get_object_or_404
 from .utils import download_certificado
 from .models import CaEPI
 from datetime import date, datetime
 from django.http import HttpResponse
+import os
 from django.core.files.base import ContentFile
+from django.conf import settings
 
 
 def caepi(request, ca):
@@ -19,20 +20,24 @@ def caepi(request, ca):
             file_content, data = download_certificado(ca=ca)
             c_a = data['CA']
             validade = datetime.strptime(data['Validade'], '%d/%m/%Y').strftime('%Y-%m-%d')
-            
             if file_content:
                 file_content_file = ContentFile(file_content)
+                
+                if caepi.pdf:
+                    old_file_path = os.path.join(settings.MEDIA_ROOT, caepi.pdf.name)
+                    if os.path.isfile(old_file_path):
+                        os.remove(old_file_path)
 
                 caepi.ca_number = c_a
                 caepi.validade = validade
                 caepi.pdf.save(f'CA{ca}.pdf', file_content_file)
                 
                 response = HttpResponse(caepi.pdf, content_type='application/pdf')
-                response['Content-Disposition'] = f'attachment; filename="{caepi.pdf.name}"'
+                response['Content-Disposition'] = f'attachment; filename="{caepi.pdf}"'
                 return response
             
         response = HttpResponse(caepi.pdf, content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename="{caepi.pdf.name}"'
+        response['Content-Disposition'] = f'attachment; filename="{caepi.pdf}"'
         return response
     
     except CaEPI.DoesNotExist:
@@ -55,9 +60,10 @@ def caepi(request, ca):
             caepi.save()
             
             response = HttpResponse(caepi.pdf, content_type='application/pdf')
-            response['Content-Disposition'] = f'attachment; filename="{caepi.pdf.name}"'
+            response['Content-Disposition'] = f'attachment; filename="{caepi.pdf}"'
             return response
         
         else:
+            
             return HttpResponse("Não foi possível baixar o arquivo", status=500)
         
